c89a681af7ff72edc71e14313548ede84dc67a44
victorinno <rotc21@gmail.com>
2018-12-27 13:36:39 -0200

    Update IA_2.py

--- IA_2.py
+++ IA_2.py
@@ -10,15 +10,13 @@ from __future__ import absolute_import, division, print_function
 import os
 import numpy as np
 import h5py
+import keras
 
-# TensorFlow and tf.keras
-import tensorflow as tf
-from tensorflow import keras
-from keras import losses
-#import matplotlib.pyplot as plt
-from tensorflow.python.framework import ops
 
 np.random.seed(1)
+batch_size = 128
+num_classes = 10
+epochs = 12
 
 f = h5py.File('data.hdf5', 'r')
 
@@ -33,22 +31,27 @@ X_test = np.array(X_test) / 255.0
 checkpoint_path = "training_2/cp-{epoch:04d}.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
-cp_callback = tf.keras.callbacks.ModelCheckpoint(
+cp_callback = keras.callbacks.ModelCheckpoint(
     checkpoint_path, verbose=1, save_weights_only=True,
     # Save weights, every 5-epochs.
     period=5)
 
 model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(784, 3)),
-    keras.layers.Dense(128, activation=tf.nn.relu),
-    keras.layers.Dense(10, activation=tf.nn.softmax)
+    keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28,28,3)),
+    keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    keras.layers.Dropout(0.25),
+    keras.layers.Flatten(),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dropout(0.5),
+    keras.layers.Dense(num_classes, activation='softmax')
 ])
 
-model.compile(optimizer=tf.train.AdamOptimizer(), 
-              loss='sparse_categorical_crossentropy',
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
               metrics=['accuracy'])
 
-model.fit(X_train, Y_train, epochs=1500, callbacks = [cp_callback], batch_size=512)
+model.fit(X_train, Y_train, epochs=epochs, callbacks = [cp_callback], batch_size=batch_size)
 
 test_loss, test_acc = model.evaluate(X_test, Y_test)
 
